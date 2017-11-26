737e89e79804bc70833acc91b03d4e773a1ad6ce
Nayaco <1194329594@qq,com>
2017-11-27 01:00:46 +0800

    The alpha edition

--- Main.cpp
+++ Main.cpp
@@ -8,12 +8,8 @@ int Insert{
     std::string Antstr,Bananastr;
     sRead(Antstr);
     sRead(Bananastr);
-<<<<<<< HEAD
     //Ant::Showme(Antstr);
     Ant::KMP(Antstr,Bananastr)!=nullptr?sWrite(1):sWrite(0);
-=======
-    Ant::Showme(Antstr);
->>>>>>> 888363591bd0d4ffc7c9f0ba4b36f9a76d5fd6f2
     Stop(S_T);
     Closew;
     return 0;
