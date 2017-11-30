5e5ae348fd790f2ba8ad019f8533023759e4bd35
Nayaco <1194329594@qq,com>
2017-11-30 23:32:35 +0800

    Alpha 0.01

--- Reader.cpp
+++ Reader.cpp
@@ -1,52 +1,9 @@
-#pragma G++ diagnostic error "-std=c++11" 
-#include<cstdio>
-#include<cstdlib>
-#include<cstring>
-#include<cmath>
-#include<map>
-#include<queue>
-#include<vector>
-#include<stack>
-#include<iostream>
-#include<algorithm>
-#include<functional>
-#include<bitset>
-#include<string>
-#include<time.h>
-#include<Windows.h>
-//defines
-#define Read(x) scanf("%s",&x)
-#define Write(x) printf("%s",x)
-#define sRead(x) cin>>x
-#define sWrite(x) cout<<x<<endl
-#define RPoint_to(x) freopen(x,"r",stdin)
-#define WPoint_to(x) freopen(x,"w",stdout)
-#define Closer fclose(stdin)
-#define Closew fclose(stdout)
-#define Allclose fclose(stdin);fclose(stdout)
-#define Setr srand((unsigned)time(NULL))
-#define ran(x) rand()%x
-#define Turnoff std::ios::sync_with_stdio(false)
-#define Start(x) x=time(NULL)
-#define Stop(x) printf("TimeCost:%d\n",time(NULL)-x);system("Pause")
-#define Insert main()
-using namespace std;
+#include<Reader.h>
 
-class Ant{
-    Ant(){
-    }
-    ~Ant(){
-    }
-    public:
-        static void Showme(string A);  
-        static int* KMP(string A,string B);
-        static int* Getnext(string A);
-    private:
-        void Selfcheck();
-};
 void Ant::Showme(string A){
     sWrite(A);
 }
+
 int* Ant::Getnext(string A){
     int *Index = new int[A.length()+1];
     Index[0] = Index[1] = 0;
@@ -60,6 +17,7 @@ int* Ant::Getnext(string A){
 	}
     return Index;
 }
+
 int* Ant::KMP(string A,string B){
     int *Index = Getnext(B);
     int *Match = new int[A.length()];
@@ -78,17 +36,6 @@ int* Ant::KMP(string A,string B){
     return Isfound?Match:nullptr;
 }
 
-class Ape{
-    public:
-        //string *Str;
-        Ape(int Num){
-            //Str = new string[Num];
-        }
-        ~Ape(){
-            sWrite("End of Ape");
-        }
-        static int* Find(string Origin,int X){
-            //return Ant::KMP(str[X]);
-        }
-};
-
+int* Ape::Find(string origin, int x){
+    return nullptr;
+}
--- Reader.h
+++ Reader.h
@@ -1,4 +1,61 @@
 #ifndef READER_H
 #define READER_H
+#pragma G++ diagnostic error "-std=c++11"
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<cmath>
+#include<map>
+#include<queue>
+#include<vector>
+#include<stack>
+#include<iostream>
+#include<algorithm>
+#include<functional>
+#include<bitset>
+#include<string>
+#include<time.h>
 
+//defines
+#define Read(x) scanf("%s",&x)
+#define Write(x) printf("%s",x)
+#define sRead(x) cin>>x
+#define sWrite(x) cout<<x<<endl
+#define RPoint_to(x) freopen(x,"r",stdin)
+#define WPoint_to(x) freopen(x,"w",stdout)
+#define Closer fclose(stdin)
+#define Closew fclose(stdout)
+#define Allclose fclose(stdin);fclose(stdout)
+#define Setr srand((unsigned)time(NULL))
+#define ran(x) rand()%x
+#define Turnoff std::ios::sync_with_stdio(false)
+#define Start(x) x=time(NULL)
+#define Stop(x) printf("TimeCost:%d\n",time(NULL)-x);system("Pause")
+#define Insert main()
+
+using namespace std;
+
+class Ant{
+    Ant(){
+    }
+    ~Ant(){
+    }
+    public:
+        static void Showme(string A);  
+        static int* KMP(string A,string B);
+        static int* Getnext(string A);
+    private:
+        void Selfcheck();
+};
+class Ape{
+    public:
+        //string *Str;
+        Ape(int Num){
+            //Str = new string[Num];
+        }
+        ~Ape(){
+            sWrite("End of Ape");
+        }
+        static int* Find(string Origin,int X);
+};
 #endif
